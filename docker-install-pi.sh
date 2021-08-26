c3cd3f516ae8c80c30bfb8328cf413cfdaa99279
Amelia Froemming <amelia@froemming.me>
2021-08-26 19:20:16 +0100

    first commit

---
+++ docker-install-pi.sh
@@ -0,0 +1,7 @@
+sudo apt-get update && sudo apt-get upgrade
+curl -sSL https://get.docker.com | sh
+sudo apt-get install libffi-dev libssl-dev
+sudo apt-get install libffi-dev libssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
+sudo systemctl enable docker
